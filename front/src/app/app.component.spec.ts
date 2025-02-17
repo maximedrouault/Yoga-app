91daf69cfc9dbafbfd89f519e793e45426faeb38
maximedrouault <maximedrouault@gmail.com>
2025-02-17 18:22:44 +0100

    U.T. added for app.component to handle user login state and logout functionality, improving test coverage

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,14 +1,26 @@
-import { HttpClientModule } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterTestingModule } from '@angular/router/testing';
-import { expect } from '@jest/globals';
+import {HttpClientModule} from '@angular/common/http';
+import {TestBed} from '@angular/core/testing';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {RouterTestingModule} from '@angular/router/testing';
+import {expect, it} from '@jest/globals';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {AuthService} from "./features/auth/services/auth.service";
+import {SessionService} from "./services/session.service";
+import {Router} from "@angular/router";
+import {of} from "rxjs";
 
 
 describe('AppComponent', () => {
+  let mockAuthService;
+  let mockSessionService: any;
+  let mockRouter: any;
+
   beforeEach(async () => {
+    mockAuthService = jest.fn();
+    mockSessionService = { $isLogged: jest.fn(), logOut: jest.fn() };
+    mockRouter = { navigate: jest.fn() };
+
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
@@ -18,6 +30,11 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: Router, useValue: mockRouter }
+      ]
     }).compileComponents();
   });
 
@@ -25,5 +42,38 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+
+  describe('AppComponent test suite', () => {
+    it('should return true when user is logged in', () => {
+      mockSessionService.$isLogged.mockReturnValue(of(true));
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+
+      app.$isLogged().subscribe(isLogged => {
+        expect(isLogged).toBeTruthy();
+      });
+    });
+
+    it('should return false when user is not logged in', () => {
+      mockSessionService.$isLogged.mockReturnValue(of(false));
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+
+      app.$isLogged().subscribe(isLogged => {
+        expect(isLogged).toBeFalsy();
+      });
+    });
+
+    it('should log out and navigate to home', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+
+      app.logout();
+
+      expect(mockSessionService.logOut).toHaveBeenCalled();
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['']);
+    });
   });
 });
