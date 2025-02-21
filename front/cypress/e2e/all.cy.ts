7a9db3583c1295fd7e5832189b246e742fdfca1e
maximedrouault <maximedrouault@gmail.com>
2025-02-21 10:46:32 +0100

    Add E2E tests for guard functionality, improving test coverage

---
+++ front/cypress/e2e/all.cy.ts
@@ -0,0 +1,7 @@
+import './form.cy';
+import './login.cy';
+import './me.cy';
+import './not-found.cy';
+import './register.cy';
+import './session.cy';
+import './sessions.cy';
---
+++ front/cypress/e2e/guard.cy.ts
@@ -0,0 +1,36 @@
+describe('Guard spec', () => {
+  const urls = {
+    loginUrl: '/login',
+    sessionsUrl: '/sessions',
+    loginApi: '/api/auth/login',
+    sessionsApi: '/api/session'
+  };
+
+  const selectors = {
+    emailInput: '[data-testid="email-input"]',
+    passwordInput: '[data-testid="password-input"]',
+    submitButton: '[data-testid="submit-button"]'
+  };
+
+  const login = (loginFixture: string) => {
+    cy.intercept('POST', urls.loginApi, { fixture: loginFixture });
+    cy.intercept('GET', urls.sessionsApi, { fixture: 'sessions' });
+
+    cy.visit(urls.loginUrl);
+    cy.get(selectors.emailInput).type("yoga@studio.com");
+    cy.get(selectors.passwordInput).type("test!1234");
+    cy.get(selectors.submitButton).click();
+  };
+
+  it('should redirect to session list when user is logged', () => {
+    login('adminLoginResponse');
+
+    cy.url().should('include', urls.sessionsUrl);
+  });
+
+  it('should redirect to login page when user is not logged', () => {
+    cy.visit(urls.sessionsUrl);
+
+    cy.url().should('include', urls.loginUrl);
+  });
+});
